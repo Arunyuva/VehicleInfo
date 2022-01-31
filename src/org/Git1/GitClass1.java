49abd73e17f959b845b9faa56dd9e3ded44be9cf
Arunyuva <arunyuva9313@gmail.com>
2022-01-31 19:52:41 +0530

    VehicleInfo

--- src/org/Git1/GitClass1.java
+++ src/org/Git1/GitClass1.java
@@ -2,4 +2,11 @@ package org.Git1;
 
 public class GitClass1 {
 
+	private void gear() {
+System.out.println("5gears in Lorry");
+	}
+	
+	private void engine() {
+System.out.println("it will be BS VI model");
+	}
 }
